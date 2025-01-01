474ad5cde071750959b4f59901e70949e6aeb972
Nanae-j <nanae9577@gmail.com>
2025-01-01 21:19:01 +0900

    [add]imagesの追加

---
+++ src/app/components/Logos/Logo.tsx
@@ -0,0 +1,94 @@
+const SvgIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1405"
+    height="1426"
+    fill="none"
+  >
+    <g filter="url(#a)">
+      <path
+        fill="#D53DFB"
+        fillOpacity="0.2"
+        d="M281 975a526 526 0 0 1 180-736 549 549 0 0 1 495-39c18 7 25 28 18 45l-62 157c-7 18-27 27-45 20-93-37-186-45-282 15-148 92-178 257-88 403 91 145 254 191 401 99 88-54 132-136 142-246 2-20 19-35 39-34l166 13c19 1 34 17 33 36a532 532 0 0 1-255 430 527 527 0 0 1-742-163"
+      ></path>
+    </g>
+    <g filter="url(#b)">
+      <path
+        fill="#2E98A4"
+        fillOpacity="0.5"
+        d="M390 907a389 389 0 0 1 134-545c107-67 250-79 366-29 13 5 19 20 14 33l-46 116c-5 14-20 21-33 16-69-28-138-34-210 11-109 68-132 190-64 298 67 107 187 142 296 74 66-41 98-102 106-183 1-15 13-26 28-25l123 9c14 1 25 13 25 27-6 130-75 247-190 318a390 390 0 0 1-549-120"
+      ></path>
+    </g>
+    <g filter="url(#c)">
+      <path
+        fill="#F8FFAA"
+        fillOpacity="0.3"
+        d="M458 865a305 305 0 0 1 104-427c85-53 196-62 288-23 10 4 14 16 10 26l-36 91c-4 11-15 16-26 12-54-21-108-26-164 9-85 53-103 149-51 233 53 85 148 112 233 58 51-32 77-79 82-143 2-11 11-20 23-19l96 7c11 1 20 10 19 21-4 102-58 193-148 249a306 306 0 0 1-430-94"
+      ></path>
+    </g>
+    <defs>
+      <filter
+        id="a"
+        width="1239.7"
+        height="1227.1"
+        x="118.4"
+        y="78.2"
+        colorInterpolationFilters="sRGB"
+        filterUnits="userSpaceOnUse"
+      >
+        <feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
+        <feBlend
+          in="SourceGraphic"
+          in2="BackgroundImageFix"
+          result="shape"
+        ></feBlend>
+        <feGaussianBlur
+          result="effect1_foregroundBlur_160_172"
+          stdDeviation="40"
+        ></feGaussianBlur>
+      </filter>
+      <filter
+        id="b"
+        width="959.5"
+        height="950.1"
+        x="249.3"
+        y="222.1"
+        colorInterpolationFilters="sRGB"
+        filterUnits="userSpaceOnUse"
+      >
+        <feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
+        <feBlend
+          in="SourceGraphic"
+          in2="BackgroundImageFix"
+          result="shape"
+        ></feBlend>
+        <feGaussianBlur
+          result="effect1_foregroundBlur_160_172"
+          stdDeviation="40"
+        ></feGaussianBlur>
+      </filter>
+      <filter
+        id="c"
+        width="786.4"
+        height="779"
+        x="330.1"
+        y="311"
+        colorInterpolationFilters="sRGB"
+        filterUnits="userSpaceOnUse"
+      >
+        <feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
+        <feBlend
+          in="SourceGraphic"
+          in2="BackgroundImageFix"
+          result="shape"
+        ></feBlend>
+        <feGaussianBlur
+          result="effect1_foregroundBlur_160_172"
+          stdDeviation="40"
+        ></feGaussianBlur>
+      </filter>
+    </defs>
+  </svg>
+);
+
+export default SvgIcon;
---
+++ src/app/components/Logos/MenuLogo.tsx
@@ -0,0 +1,43 @@
+const SvgIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="164" height="20" fill="none">
+    <g clipPath="url(#a)">
+      <path
+        fill="#fff"
+        d="M154 15h1l3 2 2-1-2-2c-3 0-5-1-5-4s2-4 5-4l5 2v1l-2 1h-1l-2-1-2 1 2 1c3 1 5 2 5 5 0 2-2 4-5 4l-6-3 1-1zm-17 0V7l1-1h2l1 1v7l2 2c2 0 3-1 3-2V7l1-1h2l1 1v12h-4v-1l-4 2c-3 0-5-2-5-5m-11-3-4-5 1-1h3l2 3h1l2-3h3l1 1-4 5v1l4 5-1 1h-2l-3-3h-1l-2 3h-3l-1-1 4-5zm-18 1c0-4 3-7 7-7s6 2 6 6v2h-9v1l3 2 3-2h1l1 1 1 1-6 3c-4 0-7-3-7-7"
+      ></path>
+      <path
+        fill="#93CA88"
+        d="M117 11h1l-3-2c-1 0-3 0-3 2zm-28 8V2l1-1h3l9 12 1-1V2l1-1h2l1 1v17l-1 1h-3L94 9h-1v11h-3z"
+      ></path>
+      <path
+        fill="#fff"
+        d="M88 19V1h4l9 11h1V1h4v18h-4L93 8h-1v11zM75 6h2l1 1v1l3-2h1l1 1v2h-2c-2 0-3 1-3 4v6h-4V7zm-16 7c0-4 3-7 7-7 3 0 6 2 6 6v2h-9v1l3 2 3-2h1l1 1v1c-1 2-3 3-5 3-4 0-7-3-7-7"
+      ></path>
+      <path fill="#93CA88" d="M68 11h1l-3-2c-2 0-3 0-3 2z"></path>
+      <path
+        fill="#fff"
+        d="M44 13c0-4 3-7 7-7s7 2 7 6v1l-1 1h-8l-1 1 3 2c2 0 3-1 3-2h1l2 1v1c-1 2-4 3-6 3-4 0-7-3-7-7"
+      ></path>
+      <path fill="#93CA88" d="M53 11h1l-3-2c-1 0-2 0-3 2z"></path>
+      <path
+        fill="#fff"
+        d="M36 6h2l1 1v1l3-2h2v3h-2c-1 0-3 1-3 4v6h-4V7zM20 16c0-3 3-5 6-5l3 1v-1c0-1-1-2-3-2l-3 1h-1l-1-1V8l5-2c4 0 7 2 7 5v8h-4v-1l-4 2c-3 0-5-2-5-4"
+      ></path>
+      <path
+        fill="#93CA88"
+        d="m24 15 2 2 3-2v-1h-3zM1 10c0-5 4-9 10-9 3 0 6 1 8 4v1l-3 1q0 1.5 0 0l-5-2c-3 0-5 2-5 5s2 6 5 6c2 0 3-1 5-3h1l2 2v1c-2 2-5 4-8 4-6 0-10-4-10-10"
+      ></path>
+      <path
+        fill="#fff"
+        d="M0 10C0 4 4 0 10 0c3 0 6 2 8 4v1l-3 2c-2-2-3-3-5-3-3 0-5 3-5 6s2 5 5 5l5-2h1l2 1v1c-2 3-5 4-8 4-6 0-10-4-10-9"
+      ></path>
+    </g>
+    <defs>
+      <clipPath id="a">
+        <path fill="#fff" d="M0 0h163v20H0z"></path>
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export default SvgIcon;
