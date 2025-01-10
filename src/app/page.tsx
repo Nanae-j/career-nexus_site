0f1dddb0f5b7e899d229cbf59f7e3ac1ca77dd09
Nanae-j <nanae9577@gmail.com>
2025-01-10 22:33:46 +0900

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import { getMembersList, getNewsList } from "./_libs/microcms";
 export default async function Home() {
   const TOP_NEWS_LIMIT = 3;
 
-  const data = await getNewsList({ limit: TOP_NEWS_LIMIT });
+  const news_data = await getNewsList({ limit: TOP_NEWS_LIMIT });
 
   const member_data = await getMembersList({ limit: 50 });
 
-  return <Top news={data.contents} members={member_data.contents} />;
+  return <Top news={news_data.contents} members={member_data.contents} />;
 }
